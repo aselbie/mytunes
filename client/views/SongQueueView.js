c141be347f544446128de2f9975d9ed71f4533b5
aselbie <aselbie@gmail.com>
2014-09-24 16:39:21 -0700

    All song queueueueueue related specs passing

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,21 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.render();
   },
 
-  render: function() {
-    return this.$el;
+  render: function(){
+
+    this.$el.children().detach();
+
+    this.$el.html('<th>Library</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
